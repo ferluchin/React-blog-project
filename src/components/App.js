dfdf778922a9d0b828804685092580e14bd46bca
ferluchin <lgrandab@gmail.com>
2021-06-20 14:46:42 -0500

    first commit react project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const App = () => {
+    const myItem = "Luis"
+
+    return (
+        <ul>
+            <li>item1</li>
+            <li>item255</li>
+            <li>{myItem}</li>
+
+        </ul>
+    )
+}
